ed371259ec0028c5b029f33c1446ce976e421815
AghnatHs <aghnathasya@gmail.com>
2025-03-05 23:12:14 +0700

    Add class surat init: need further dev

--- main.cpp
+++ main.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
+
+#include "surat.h"
 using namespace std;
 
-class Surat {
-   private:
-    string id;
-    string date;
-    string content;
-    string status;
-
-   public:
-    Surat(string id, string date, string content, string status) {
-        this->id = id;
-        this->date = date;
-        this->content = content;
-        this->status = status;
-    };
-    string toString() { return this->id; };
-};
-
-int main() {
-    Surat surat("test", "test", "test", "Test");
-    cout << surat.toString() << endl;
-}
+int main() {}
